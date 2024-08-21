d03699bd21c308d7855f62c4c1ae29e16c8665e4
Sergiodmj <sergiomazutti@gmail.com>
2024-08-20 22:19:11 -0300

    new file:   src/app/coponents/loginForm.tsx 	modified:   src/app/page.tsx 	new file:   src/app/page/home/page.tsx

---
+++ src/app/coponents/loginForm.tsx
@@ -0,0 +1,32 @@
+import { redirect } from "next/navigation";
+
+export default function LoginForm(props: any) {
+  async function Login(form: FormData) {
+    "use server";
+    const credenciais = Object.fromEntries(form);
+    //   console.log(credenciais)
+    const response = await fetch("https://la.sitesdahora.com.br/api/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: credenciais.email,
+        password: credenciais.password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.status !== 200) return null;
+
+    const user = await response.json();
+      console.log(user);
+
+    redirect("/page/home");
+  }
+
+  return (
+    <form action={Login} method="POST">
+      <input name="email" />
+      <input name="password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
